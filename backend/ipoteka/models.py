8901b8dd7c5867ef4c6e14f7f2ba03b903d0c50b
oimatewtfff <gavrilyuk.vitalya14@mail.ru>
2022-06-25 18:41:36 +0300

    appended new rows in model

--- backend/ipoteka/models.py
+++ backend/ipoteka/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.db import models
 
 
@@ -6,12 +5,12 @@ class MortgageOffers(models.Model):
     id = models.BigAutoField(primary_key=True)
     payment = models.IntegerField(default=0)
     bank_name = models.CharField(max_length=100)
-    term_min = models.IntegerField(default=0)
-    term_max = models.IntegerField(default=0)
-    rate_min = models.FloatField(default=0)
-    rate_max = models.FloatField(default=0)
-    payment_min = models.IntegerField(default=0)
-    payment_max = models.IntegerField(default=0)
+    term_min = models.IntegerField(default=0) # Срок ипотеки, ОТ
+    term_max = models.IntegerField(default=0) # Срок ипотеки, ДО
+    rate_min = models.FloatField(default=0) # Ставка, ОТ
+    rate_max = models.FloatField(default=0) # Ставка, ДО
+    payment_min = models.IntegerField(default=0) # Сумма кредита, ОТ
+    payment_max = models.IntegerField(default=0) # Сумма кредита, ДО
 
     def __str__(self):
         return self.bank_name
