5e6378d104bdb9748cfa4a7ef41a11980e6618eb
oimatewtfff <gavrilyuk.vitalya14@mail.ru>
2022-06-25 16:38:48 +0300

    included apps url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -10,6 +10,7 @@ from .routers import router
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("", include("ipoteka.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
 ]
