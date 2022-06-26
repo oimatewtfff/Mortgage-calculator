2c525820b797cc5ac4127dc25a0a105c71601f11
oimatewtfff <gavrilyuk.vitalya14@mail.ru>
2022-06-26 11:19:14 +0300

    replased serializer with model serializer

--- backend/ipoteka/serializers.py
+++ backend/ipoteka/serializers.py
@@ -2,27 +2,13 @@ from rest_framework import serializers
 from .models import MortgageOffers
 
 
-class MortgageOffersSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    payment = serializers.IntegerField()
-    bank_name = serializers.CharField()
-    term_min = serializers.IntegerField() # Срок ипотеки, ОТ
-    term_max = serializers.IntegerField() # Срок ипотеки, ДО
-    rate_min = serializers.FloatField() # Ставка, ОТ
-    rate_max = serializers.FloatField() # Ставка, ДО
-    payment_min = serializers.IntegerField() # Сумма кредита, ОТ
-    payment_max = serializers.IntegerField() # Сумма кредита, ДО
-
-    def create(self, validated_data):
-        return MortgageOffers.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.bank_name = validated_data.get('bank_name', instance.bank_name)
-        instance.term_min = validated_data.get('term_min', instance.term_min)
-        instance.term_max = validated_data.get('term_max', instance.term_max)
-        instance.rate_min = validated_data.get('rate_min', instance.rate_min)
-        instance.rate_max = validated_data.get('rate_max', instance.rate_max)
-        instance.payment_min = validated_data.get('payment_min', instance.payment_min)
-        instance.payment_max = validated_data.get('payment_max', instance.payment_max)
-        instance.save()
-        return instance
+class MortgageOffersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MortgageOffers
+        fields = ('bank_name',
+                  'rate_min',
+                  'rate_max',
+                  'term_min',
+                  'term_max',
+                  'payment_min',
+                  'payment_max')
