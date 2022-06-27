7124c5d9a485550246667ae7accdd10a91a39b97
oimatewtfff <gavrilyuk.vitalya14@mail.ru>
2022-06-27 18:06:50 +0300

    added model fields to be displayed

--- backend/ipoteka/serializers.py
+++ backend/ipoteka/serializers.py
@@ -5,7 +5,9 @@ from .models import MortgageOffers
 class MortgageOffersSerializer(serializers.ModelSerializer):
     class Meta:
         model = MortgageOffers
-        fields = ('bank_name',
+        fields = ('id',
+                  'payment',
+                  'bank_name',
                   'rate_min',
                   'rate_max',
                   'term_min',
